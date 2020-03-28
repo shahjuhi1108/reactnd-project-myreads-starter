296c8fa431dc4ec01507fcaa977d52a839f0c37e
Juhi <shahjuhi1108@gmail.com>
2020-03-27 20:56:58 -0700

    Updates

---
+++ precache-manifest.4ca2bc28ec6902b9d3f758f6db89981c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a778bb801db3aabe0169",
+    "url": "/reactnd-project-myreads-starter/static/css/main.81a40285.chunk.css"
+  },
+  {
+    "revision": "a778bb801db3aabe0169",
+    "url": "/reactnd-project-myreads-starter/static/js/main.a778bb80.chunk.js"
+  },
+  {
+    "revision": "8c8eeb116bc6cd9cc98d",
+    "url": "/reactnd-project-myreads-starter/static/js/1.8c8eeb11.chunk.js"
+  },
+  {
+    "revision": "83f7d75ca12013b90cc6",
+    "url": "/reactnd-project-myreads-starter/static/js/runtime~main.83f7d75c.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/reactnd-project-myreads-starter/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "8c0b92010b50db5716151e5ecda3cbcf",
+    "url": "/reactnd-project-myreads-starter/index.html"
+  }
+];
